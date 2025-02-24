02ae75b7138ffae4c7393e9e2daab648c0a57ee8
Joshua Thijssen <jthijssen@noxlogic.nl>
2025-02-24 12:50:42 +0100

    override esbuild

--- app/Http/Controllers/Landing/TimelineController.php
+++ app/Http/Controllers/Landing/TimelineController.php
@@ -235,8 +235,8 @@ class TimelineController extends Controller
 
         // TODO: As long as we're not using a FHIR extension to define the setup of the bundle
         // we're bound to fixed indexes to tind the resource.
-        foreach($addressingInformation['entry'][0]['resource']['identifier'] as $ura) {
-            if($ura['system'] === 'http://fhir.nl/fhir/NamingSystem/ura'){
+        foreach ($addressingInformation['entry'][0]['resource']['identifier'] as $ura) {
+            if ($ura['system'] === 'http://fhir.nl/fhir/NamingSystem/ura') {
                 return $ura['value'];
             }
         }
