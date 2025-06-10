d12c7d834ddd0cd24e41480c459c88fe4e0c76aa
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-06-10 09:38:42 +0200

    Add simple form to redirect to wallet for testing

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import '@minvws/manon/collapsible';
 // wait for the document to be loaded
 document.addEventListener('DOMContentLoaded', function() {
     bindLogoutClickHandler();
+    bindPresentCredentialForm();
 });
 
 function bindLogoutClickHandler() {
@@ -27,5 +28,32 @@ function bindLogoutClickHandler() {
 
         // submit form
         logoutForm.submit();
+    });
+}
+
+function bindPresentCredentialForm()
+{
+    // check if load credential into wallet form exists
+    if (!document.querySelector('#present-credential-form')) {
+        return
+    }
+
+    // bind form submit event
+    const form = document.querySelector('#present-credential-form');
+    form.addEventListener('submit', function (event) {
+        // prevent default behaviour
+        event.preventDefault();
+
+        // get the form data, the formdata contains wallet_url and present_credential_uri.
+        const formData = new FormData(form);
+
+        // redirect to the wallet app with the form data
+        const walletUrl = formData.get('wallet_url');
+        const credentialPresentationUri = formData.get('present_credential_uri');
+
+        if (walletUrl && credentialPresentationUri) {
+            // Strip `openid4vp://authorize`
+            window.location.href = `${walletUrl}${credentialPresentationUri.replace('openid4vp://authorize', '')}`;
+        }
     });
 }
--- resources/views/auth/vc.blade.php
+++ resources/views/auth/vc.blade.php
@@ -15,6 +15,32 @@
             <h1>@lang('VC Login - Present credential')</h1>
 
             <input readonly value="{{ $vpUrl }}"/>
+
+            <h2>Inladen in een wallet</h2>
+
+            <form class="horizontal-view" id="present-credential-form">
+                <input type="hidden" name="present_credential_uri" value="{{ $vpUrl }}">
+                <div>
+                    <label for="wallet-url">Wallet URL</label>
+                    <div>
+                        <input id="wallet-url" name="wallet_url" type="text" required>
+                    </div>
+                </div>
+                <button type="submit">Naar wallet</button>
+            </form>
+
+            <ul class="accordion">
+                <li>
+                    <button aria-expanded="false" id="debug-accordion">
+                        Debug
+                    </button>
+                    <div aria-labelledby="debug-accordion">
+                        @env('local')
+                            <p>Lokale wallet URL: http://localhost:8610/api/siop/initiatePresentation</p>
+                        @endenv
+                    </div>
+                </li>
+            </ul>
         </div>
     </section>
 @endsection
