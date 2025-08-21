39108c225444d0587ee81521e9abee747cc82310
Thomas Bosch <bosch-thomas@protonmail.com>
2025-08-21 17:24:40 +0200

    Refactor DigidMockController to use JWK for signing and create JWS instead of JWT. Uses x5t and kid isntead of x5c

--- app/Http/Controllers/Auth/DigidMockController.php
+++ app/Http/Controllers/Auth/DigidMockController.php
@@ -6,39 +6,79 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
+use Jose\Component\Core\AlgorithmManager;
+use Jose\Component\KeyManagement\JWKFactory;
+use Jose\Component\Signature\Algorithm\RS256;
+use Jose\Component\Signature\JWSBuilder;
+use Jose\Component\Signature\Serializer\CompactSerializer;
 use MinVWS\OpenIDConnectLaravel\Http\Responses\LoginResponseHandlerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use OpenSSLAsymmetricKey;
+use Jose\Component\Core\JWK;
 
 class DigidMockController extends Controller
 {
-    private readonly OpenSSLAsymmetricKey $privateKey;
-    private readonly array $x5c;
+    private readonly JWK $signingKey;
+    private readonly JWK $publicKey;
+    private readonly JWSBuilder $jwsBuilder;
+    private readonly CompactSerializer $serializer;
+    private readonly string $kid;
 
     public function __construct(
         private readonly LoginResponseHandlerInterface $loginResponseHandler
     ) {
-        // Load RSA private key and certificate from config once
-        $certificate = file_get_contents(config('auth.uzi_jwt_signing_cert'));
-        if (!$certificate) {
-            throw new \RuntimeException('UZI JWT signing certificate not configured');
+        // Load private key for signing
+        $privateKeyPath = config('auth.uzi_jwt_signing_key');
+        if (!$privateKeyPath || !file_exists($privateKeyPath)) {
+            throw new \RuntimeException('UZI JWT signing key not configured or not found');
         }
-        $privateKey = file_get_contents(config('auth.uzi_jwt_signing_key'));
-        if (!$privateKey) {
-            throw new \RuntimeException('UZI JWT signing key not configured');
+        $this->signingKey = JWKFactory::createFromKeyFile($privateKeyPath);
+
+        // Load certificate for x5c/x5t headers
+        $certificatePath = config('auth.uzi_jwt_signing_cert');
+        if (!$certificatePath || !file_exists($certificatePath)) {
+            throw new \RuntimeException('UZI JWT signing certificate not configured or not found');
         }
-        $this->privateKey = openssl_pkey_get_private($privateKey)
-             ?: throw new \RuntimeException('UZI JWT signing key not configured');
+        $this->publicKey = JWKFactory::createFromCertificateFile($certificatePath);
+        $this->kid = $this->kidFromCertificate($certificatePath);
 
-
-        $this->x5c = [base64_encode($this->convertCertToDer($certificate))];
+        // Initialize Jose components
+        $algorithmManager = new AlgorithmManager([new RS256()]);
+        $this->jwsBuilder = new JWSBuilder($algorithmManager);
+        $this->serializer = new CompactSerializer();
     }
 
-    private function createMockJwt(): string
+    /**
+     * Generate a "kid" (Key ID) from a certificate.
+     * The "kid" is a unique identifier for the key. There is no standard way to generate a kid.
+     */
+    private function kidFromCertificate(string $certificatePath): string
     {
-        $payload = [
+        $certificateContent = file_get_contents($certificatePath);
+        if ($certificateContent === false) {
+            throw new \RuntimeException('Unable to read certificate file');
+        }
+
+        // Parse the certificate
+        $certificate = openssl_x509_read($certificateContent);
+        if ($certificate === false) {
+            throw new \RuntimeException('Unable to parse certificate');
+        }
+
+        // Get the certificate fingerprint (SHA256)
+        $fingerprint = openssl_x509_fingerprint($certificate, 'sha256', true);
+        if ($fingerprint === false) {
+            throw new \RuntimeException('Unable to generate certificate fingerprint');
+        }
+
+        // Convert to URL-safe base64
+        return strtr(base64_encode($fingerprint), '+/', '-_');
+    }
+
+    private function createMockPayload(): array
+    {
+        return [
             "aud" => "",
-            "exp" => time() + 3600, // 1 hour from now
+            "exp" => time() + 3600,
             "initials" => "m",
             "iss" => "",
             "loa_authn" => "",
@@ -58,51 +98,30 @@ class DigidMockController extends Controller
             "surname_prefix" => "",
             "uzi_id" => "12312123"
         ];
-
-        // Create custom header with x5c (Firebase JWT doesn't support array values in headers)
-        $header = [
-            'alg' => 'RS256',
-            'typ' => 'JWT',
-            'x5c' => $this->x5c
-        ];
-
-        // Manually create JWT since Firebase JWT doesn't support x5c array
-        $headerEncoded = $this->base64UrlEncode(json_encode($header)
-            ?: throw new \RuntimeException('Failed to encode header'));
-        $payloadEncoded = $this->base64UrlEncode(json_encode($payload)
-            ?: throw new \RuntimeException('Failed to encode payload'));
-
-        $signatureData = $headerEncoded . '.' . $payloadEncoded;
-        $signature = '';
-
-        if (!openssl_sign($signatureData, $signature, $this->privateKey, OPENSSL_ALGO_SHA256)) {
-            throw new \RuntimeException('Failed to sign JWT with RSA key');
-        }
-
-        $signatureEncoded = $this->base64UrlEncode($signature);
-
-        return $headerEncoded . '.' . $payloadEncoded . '.' . $signatureEncoded;
     }
 
-    private function base64UrlEncode(string $data): string
+    private function createMockJws(): string
     {
-        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
-    }
+        $jws = $this->jwsBuilder
+            ->create()
+            ->withPayload(json_encode($this->createMockPayload()) ?: '')
+            ->addSignature($this->signingKey, [
+                'alg' => 'RS256',
+                'typ' => 'JWT',
+                'x5t' => rtrim($this->publicKey->get('x5t'), '='), // Remove padding
+                'kid' => $this->kid,
+            ])
+            ->build();
 
-    private function convertCertToDer(string $certificate): string
-    {
-        // Remove PEM headers and decode to get DER format
-        $cert = str_replace(['-----BEGIN CERTIFICATE-----', '-----END CERTIFICATE-----', "\n", "\r"], '', $certificate);
-        return base64_decode($cert) ?: throw new \RuntimeException('Failed to decode certificate');
+        return $this->serializer->serialize($jws, 0);
     }
-
 
     public function login(): Response
     {
         // A mock JWT is placed in session when DIGID mock is enabled
         // If normal flow with Dezi login is enabled, we don't need to do anything special:
         //      setting JWT in session is then handled by JweDecryptService
-        Session::put('oidc_jwt', $this->createMockJwt());
+        Session::put('oidc_jwt', $this->createMockJws());
         return $this->loginResponseHandler->handleLoginResponse(
             (object)[
                 "relations" => [(object)['entity_name' => "BossEntity", 'ura' => "12341234", 'roles' => ["01.000"]]],
